5f9a8e61dc140fe9fc6682fd59d67f7ea8f6655d
Jake Mathura <jakemathura@jakes-air.lan>
2024-10-20 15:01:21 -0400

    Add Resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 const Resume = () => {
     return (
-        <div className="resume">
-            <h2>Resume</h2>
-            <p>TODO</p>
+        <div>
+            <h2>My Resume</h2>
+            {/* Embed the PDF using the embed tag */}
+            <embed
+                src={`${process.env.PUBLIC_URL}/resume.pdf`}
+                type="application/pdf"
+                width="100%"
+                height="800px"
+            />
         </div>
     );
 };
